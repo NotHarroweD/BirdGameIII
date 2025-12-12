98f303fd7babe9eb78c6e89bf341e4a25cd84cab
Totally Not HarroweD <46828061+NotHarroweD@users.noreply.github.com>
2025-12-12 14:31:20 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
+/// <reference types="node" />
+
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
@@ -14,11 +13,6 @@ export default defineConfig(({ mode }) => {
       alias: {
         '@': process.cwd(),
       },
-    },
-    define: {
-      // This shim allows your existing code (process.env.API_KEY) to read the key
-      // from a .env file or Vercel environment variables.
-      'process.env.API_KEY': JSON.stringify(env.API_KEY)
     }
   }
 })
